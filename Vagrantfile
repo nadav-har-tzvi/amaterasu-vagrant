00f3f2465698bae4d3b2442e867669e070c0eab1
Nadav Har Tzvi <nadavhartzvi@gmail.com>
2017-10-01 15:16:44 +0300

    aws provisioning init script

--- Vagrantfile
+++ Vagrantfile
@@ -1,17 +1,23 @@
 # Vagrantfile for amaterasu dev station
 Vagrant.configure(2) do |config|
 
-  config.vm.box = "bento/centos-7.1"
+  config.vm.box = "js-schibsted/base-centos6"
+  config.vm.box_download_insecure=true
+  config.vm.provision :file, source: "amaterasu.properties", destination: "amaterasu.properties"
   
   config.vm.define "node1" do |node1|
-   
-    node1.vm.network "private_network", ip: "192.168.33.11"
-    node1.vm.hostname = "node1"
-    node1.vm.synced_folder "~/Work/Shinto/roadan/amaterasu/build/amaterasu", "/ama"
-           
-    node1.vm.provider "virtualbox" do |vb|
-      vb.memory = "4098"
-		  vb.cpus = 3
+
+    node1.vm.provider :aws do |aws, override|
+      #aws.access_key_id = "AKIAIK75YFTQ2ONGLAVQ"
+      #aws.secret_access_key = "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"
+      #aws.session_token = "FQoDYXdzENL//////////wEaDB3Qn4UftaHihjeXNSKsAU4IEym6vTZJ3dtUMvEc7qJtKmIVi6gaXuO9bPHfRF+Wc/vQNkY1sRRtXGKV0yliMpyaQF5QTkBBWjenU7y3BfJfL2WXAuhTepkULyc/SqOocbZJt4cCBIqJdwhf1pNhE9x3QXnxBWVavd33zRC3MYWHXyXBrdzxJ+NV6iw2A27Bda18wkZDXvbqFBxh4/JyCrZpDljY947CfKHWq3B2bAj9KmI5+c0VaEh9C7Yo2ebCzgU="
+      aws.keypair_name = "amaterasu-new"
+      aws.ami = "ami-7cbc6e13"
+      aws.instance_type = "c4.xlarge"
+      aws.region = "eu-central-1"
+      aws.security_groups = ["launch-wizard-3"]
+      override.ssh.username = "centos"
+      override.ssh.private_key_path = "C:\\Users\\nadavh\\Downloads\\amaterasu-new.pem"
     end
 
     node1.vm.provision "shell", run: "always" do |s|
--- provision.sh
+++ provision.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 amaConfDir="/etc/amaterasu"
-mesosInstalled=`yum list instaled mesos`
-marathonInstalled=`yum list instaled marathon`
-zookeeperInstalled=`yum list instaled zookeeper`
-zookeeperServerInstalled=`yum list instaled zookeeper-server`
-internalIp=`python aws_tools.py private_ip master`
+mesosInstalled=`yum list installed mesos 2>&1 >/dev/null`
+marathonInstalled=`yum list installed marathon 2>&1 >/dev/null`
+zookeeperInstalled=`yum list installed zookeeper 2>&1 >/dev/null`
+zookeeperServerInstalled=`yum list installed zookeeper-server 2>&1 >/dev/null`
+internalIp=`curl 169.254.169.254/latest/meta-data/local-ipv4`
 noPkg="Error: No matching Packages to list"
 origHome=/home/`logname`
+
+sudo mkdir /etc/amaterasu
+sudo chmod 777 -R /etc/amaterasu
 
 echo "Preparing configuration"
 if [ ! -f ${amaConfDir}/hosts.bak ]; then
@@ -15,21 +18,13 @@ if [ ! -f ${amaConfDir}/hosts.bak ]; then
 fi
 echo "hosts file backed up in ${amaConfDir}/hosts.bak"
 
-if [ ! -f ${amaConfDir}/mesos.bak ]; then
-  cp /etc/default/mesos ${amaConfDir}/mesos.bak
-fi
-echo "Mesos config file backed up in ${amaConfDir}/mesos.bak"
-
 sudo cp ${amaConfDir}/hosts.bak /etc/hosts
 echo "Restored hosts file to original state"
-
-sudo cp ${amaConfDir}/mesos.bak /etc/default/mesos
-echo "Restored mesos config to original state"
 
 echo "Setting up the hosts file"
 echo "${internalIp} node1" >> /etc/hosts
 
-if [ ${mesosInstalled} -eq ${noPkg} ] || [ ${marathonInstalled} -eq ${noPkg} ]; then
+if [ "${mesosInstalled}" == "${noPkg}" ] || [ "${marathonInstalled}" == "${noPkg}" ]; then
   echo "installing mesos and marathon"
   echo "----------------------------"
   rpm -Uvh http://repos.mesosphere.com/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm
@@ -37,6 +32,14 @@ if [ ${mesosInstalled} -eq ${noPkg} ] || [ ${marathonInstalled} -eq ${noPkg} ]; 
 else
   echo "mesos and marathon already installed"
 fi
+
+if [ ! -f ${amaConfDir}/mesos.bak ]; then
+  cp /etc/default/mesos ${amaConfDir}/mesos.bak
+fi
+echo "Mesos config file backed up in ${amaConfDir}/mesos.bak"
+
+sudo cp ${amaConfDir}/mesos.bak /etc/default/mesos
+echo "Restored mesos config to original state"
 
 echo "IP=${internalIp}" >> /etc/default/mesos
 sudo rm -f /etc/mesos/ip
@@ -52,7 +55,7 @@ source ${origHome}/.bashrc
 sudo chmod 777 -R /etc/mesos-slave/
 touch /etc/mesos-slave/?no-switch_user
 
-if [ ${zookeeperInstalled} -eq ${noPkg} ] || [ ${zookeeperServerInstalled} -eq ${noPkg} ]; then
+if [ "${zookeeperInstalled}" == "${noPkg}" ] || [ "${zookeeperServerInstalled}" == "${noPkg}" ]; then
   echo "installing zookeeper"
   echo "--------------------"
 
@@ -70,3 +73,30 @@ echo "--------------"
 
 service mesos-master start
 service mesos-slave start
+
+echo "Installing git"
+echo "--------------"
+yum -y install git
+echo "Installing java-devel"
+echo "--------------"
+yum -y install java-1.8.0-openjdk-devel
+echo "Installing wget"
+echo "--------------"
+yum -y install wget
+echo "Installing bzip2"
+echo "--------------"
+yum -y install bzip2
+
+echo "zk=${internalIp}" >> ${origHome}/amaterasu.properties
+echo "master=${internalIp}" >> ${origHome}/amaterasu.properties
+
+git clone https://github.com/shintoio/amaterasu.git
+cd ${origHome}/amaterasu
+./gradlew buildDistribution
+./gradlew buildDistribution
+
+chmod -R 777 ${origHome}/amaterasu
+
+ln -s ${origHome}/amaterasu/build/amaterasu /ama
+
+cp ${origHome}/amaterasu.properties /ama/amaterasu.properties
